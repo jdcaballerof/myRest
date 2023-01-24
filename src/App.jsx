84ca19c9602602b64bde0ab3549398a746c1b415
David CF <jcaballero@RTP.GOB.MX>
2023-01-24 09:42:47 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Hiiii</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quidem repellendus molestiae quae dignissimos nostrum provident ullam dolorem perspiciatis soluta recusandae, in quod tempora mollitia voluptate optio enim. Nobis, minima!</p>
+    </div>
+  )
+}
+
+export default App
